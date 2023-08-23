cf245c8b11b384854f5721e28cee8152bba3c70a
Sergey <semakin.s.v@gmail.com>
2023-08-23 13:36:02 +0300

    task 1 done

--- hw4/hw4.sql
+++ hw4/hw4.sql
@@ -1,2 +1,5 @@
 USE seminar4; 
-
+-- Подсчитать общее количество лайков, которые получили пользователи младше 12 лет.
+SELECT p.user_id, p.birthday 
+FROM `profiles` AS p
+WHERE YEAR(NOW()) - YEAR(p.birthday) < 12;
