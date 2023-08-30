beae31b3cfee1baaa52d679eae7424483bcd3d1c
Sergey <semakin.s.v@gmail.com>
2023-08-30 15:07:21 +0300

    task hw5_task3 done

--- hw5/hw5.sql
+++ hw5/hw5.sql
@@ -1,5 +1,6 @@
 USE seminar4;
-
+/*Создайте представление, в которое попадет информация о пользователях (имя, фамилия,
+город и пол), которые не старше 20 лет*/
 CREATE VIEW young_users AS
 	(SELECT firstname, lastname, hometown, gender
 	FROM users
@@ -8,4 +9,12 @@ CREATE VIEW young_users AS
    
    SELECT * FROM young_users;
    
-   
+/*Выберите все сообщения, отсортируйте сообщения по возрастанию даты отправления
+(created_at) и найдите разницу дат отправления между соседними сообщениями, получившегося
+списка. (используйте LEAD или LAG)*/
+SELECT id, body, created_at,
+LAG(created_at) OVER time_created_at AS previous,
+LEAD(created_at) OVER time_created_at AS next
+FROM messages
+WINDOW time_created_at AS (ORDER BY created_at)
+ORDER BY created_at;
